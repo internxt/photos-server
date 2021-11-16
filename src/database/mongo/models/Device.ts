9d044f8d01827a6d3455172794864a819e26b76e
sergiogutierrezvillalba <serguvil@upv.edu.es>
2021-11-16 21:24:12 +0100

    make _id required at DeviceDocument

--- src/database/mongo/models/Device.ts
+++ src/database/mongo/models/Device.ts
@@ -1,8 +1,8 @@
 import { ObjectId } from 'mongodb';
 import { Device } from '../../../models/Device';
 
-export interface DeviceDocument extends Device {
-  _id?: ObjectId
+export interface DeviceDocument extends Omit<Device, 'id'> {
+  _id: ObjectId
   createdAt: Date,
   updatedAt: Date
 }
