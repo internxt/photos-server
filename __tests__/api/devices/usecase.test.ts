6249d3e35d1b11a84f3e9df496d10e6155833b3d
sergiogutierrezvillalba <serguvil@upv.edu.es>
2021-11-16 19:05:44 +0100

    install sinon + device usecase tests

---
+++ __tests__/api/devices/usecase.test.ts
@@ -0,0 +1,65 @@
+import { Collection } from 'mongodb';
+import { stub } from 'sinon';
+
+import { DevicesRepository } from '../../../src/api/devices/repository';
+import { DevicesUsecase } from '../../../src/api/devices/usecase';
+import { Device } from '../../../src/models/Device';
+
+const CollectionStubbed = stub(Collection, 'prototype').returns(Collection);
+let repository: DevicesRepository;
+let usecase: DevicesUsecase;
+
+beforeEach(() => {
+  repository = new DevicesRepository(CollectionStubbed());
+  usecase = new DevicesUsecase(repository);
+});
+
+describe('Devices usecases', () => {
+  it('obtainDevice()', async () => {
+    const deviceId = 'deviceId';
+    const deviceDoc: Device = {
+      id: deviceId,
+      mac: '',
+      name: '',
+      userUuid: ''
+    };
+
+    stub(repository, 'getById').returns(Promise.resolve(deviceDoc));
+
+    const device = await usecase.obtainDevice(deviceId);
+
+    expect(device).toStrictEqual(deviceDoc);
+  });
+
+  it('saveDevice()', async () => {
+    const expected = 'deviceId';
+    const deviceDoc: Device = {
+      id: expected,
+      mac: '',
+      name: '',
+      userUuid: ''
+    };
+
+    stub(repository, 'create').returns(Promise.resolve(expected));
+
+    const received = await usecase.saveDevice(deviceDoc);
+
+    expect(received).toStrictEqual(expected);
+  });
+
+  it('removeDevice()', async () => {
+    const deviceId = 'deviceId';
+    const deviceDoc: Device = {
+      id: '',
+      mac: '',
+      name: '',
+      userUuid: ''
+    };
+
+    stub(repository, 'getById').returns(Promise.resolve(deviceDoc));
+
+    const device = await usecase.obtainDevice(deviceId);
+
+    expect(device).toStrictEqual(deviceDoc);
+  });
+});
