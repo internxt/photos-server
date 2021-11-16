9c19f948a758ef19dbde172404772c80de4c8b89
sergiogutierrezvillalba <serguvil@upv.edu.es>
2021-11-16 22:33:04 +0100

    make photo id required

--- src/database/mongo/models/Photo.ts
+++ src/database/mongo/models/Photo.ts
@@ -2,8 +2,8 @@ import { ObjectId } from 'mongodb';
 
 import { Photo } from '../../../models/Photo';
 
-export interface PhotoDocument extends Photo {
-  _id?: ObjectId,
+export interface PhotoDocument extends Omit<Photo, 'id'> {
+  _id: ObjectId,
   createdAt: Date,
   updatedAt: Date
 }
