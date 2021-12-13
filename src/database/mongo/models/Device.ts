658ed3ae8ed5f42295be8e7c58594dcb7e495933
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-12-13 09:32:39 +0100

    objectId type for mongo document types

--- src/database/mongo/models/Device.ts
+++ src/database/mongo/models/Device.ts
@@ -1,8 +1,9 @@
 import { ObjectId } from 'mongodb';
 import { Device } from '../../../models/Device';
 
-export interface DeviceDocument extends Omit<Device, 'id'> {
+export interface DeviceDocument extends Omit<Device, 'id' | 'userId'> {
   _id: ObjectId;
+  userId: ObjectId;
   createdAt: Date;
   updatedAt: Date;
 }
--- src/database/mongo/models/Photo.ts
+++ src/database/mongo/models/Photo.ts
@@ -2,8 +2,10 @@ import { ObjectId } from 'mongodb';
 
 import { Photo } from '../../../models/Photo';
 
-export interface PhotoDocument extends Omit<Photo, 'id'> {
+export interface PhotoDocument extends Omit<Photo, 'id' | 'userId' | 'deviceId'> {
   _id: ObjectId;
+  userId: ObjectId;
+  deviceId: ObjectId;
   createdAt: Date;
   updatedAt: Date;
 }
