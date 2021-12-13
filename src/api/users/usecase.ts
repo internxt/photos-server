9c7efc55aef59191494269af1c582b6598517f04
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-12-13 12:38:13 +0100

    create user before device on user init + rollback user init improved message description

--- src/api/users/usecase.ts
+++ src/api/users/usecase.ts
@@ -1,4 +1,5 @@
 import { Environment } from '@internxt/inxt-js';
+import { EnvironmentConfig } from '@internxt/inxt-js/build/api';
 
 import { Device, DeviceId } from '../../models/Device';
 import { User, UserId } from '../../models/User';
@@ -28,48 +29,65 @@ export class UsersUsecase {
     return !!maybeUser;
   }
 
-  async initUser(userId: UserId, deviceInfo: Omit<Device, 'id' | 'userId'>): Promise<UserId> {
-    const userAlreadyExists = await this.isUserAlreadyInitialized(userId);
+  async initUser(
+    uuid: string, 
+    config: Pick<EnvironmentConfig, 'bridgeUser' | 'bridgePass'>, 
+    deviceInfo: Omit<Device, 'id' | 'userId'>
+  ): Promise<UserId> {
+    const userAlreadyExists = await this.isUserAlreadyInitialized(uuid);
 
     if (userAlreadyExists) {
       throw new Error('User already exists');
     }
 
-    let deviceId: DeviceId = "";
-    let bucketId: string = "";
+    let deviceId: DeviceId = '';
+    let userId: UserId = '';
+    let bucketId = '';
 
-    const network = new Environment({
-      bridgePass: '',
-      bridgeUser: '',
-      bridgeUrl: process.env.NETWORK_URL
-    });
+    const network = new Environment({ ...config, bridgeUrl: process.env.NETWORK_URL });
 
     try {
-      bucketId = await network.createBucket(`Photos-${userId}`);
+      bucketId = await network.createBucket(`Photos-${uuid}`);
 
-      const newDevice: Omit<Device, 'id'> = { ...deviceInfo, userId };
+      const newUser: Omit<User, 'id'> = { uuid, bucketId };
+      userId = await this.usersRepository.create(newUser);
+
       // TODO: Does this device already exist? (look by name?? index name???)
+      const newDevice: Omit<Device, 'id'> = { ...deviceInfo, userId };
       deviceId = await this.devicesRepository.create(newDevice);
 
-      const newUser: User = { id: userId, bucketId };
-
-      return this.usersRepository.create(newUser);
+      return userId;
     } catch (err) {
-      const bucketWasInitialized = bucketId !== "";
-      const deviceWasInitialized = deviceId !== "";
+      const bucketWasInitialized = bucketId !== '';
+      const deviceWasInitialized = deviceId !== '';
+      const userWasInitialized = userId !== '';
+
+      let finalErrorMessage = 'Error initializing user: ' + (err as Error).message;
 
       if (bucketWasInitialized) {
-        await this.rollbackUserInitialization(userId, bucketId, deviceWasInitialized ? deviceId : null);
+        const rollbackError = await this.rollbackUserInitialization(
+          bucketId, 
+          userWasInitialized ? userId : null, 
+          deviceWasInitialized ? deviceId : null
+        );
+
+        if (rollbackError) {
+          finalErrorMessage += `and ${rollbackError.message}`;
+        }
       }
 
-      throw err;
+      throw new Error(finalErrorMessage);
     }
   }
 
   /**
    * Rollbacks an unfinished user initialization with a best effort approach
    */
-  private async rollbackUserInitialization(userId: UserId, bucketId: string, deviceId: string | null) {
+  private async rollbackUserInitialization(
+    bucketId: string, 
+    userId: UserId | null, 
+    deviceId: string | null
+  ): Promise<Error | null> {
     const network = new Environment({
       bridgePass: '',
       bridgeUser: '',
@@ -77,17 +95,29 @@ export class UsersUsecase {
     });
 
     let bucketDeleted = false;
+    let userDeleted = false;
 
     try {
       await network.deleteBucket(bucketId);
       bucketDeleted = true;
 
+      if (userId) {
+        await this.usersRepository.deleteById(userId);
+        userDeleted = true;
+      }
+
       if (deviceId) {
         await this.devicesRepository.deleteById(deviceId);
       }
+
+      return null;
     } catch (err) {
-      const message = `User init failed (bucket deleted: ${bucketDeleted ? 'yes' : 'no'}) for user ${userId} due to: ${(err as Error).message}`;
-      throw new Error(message);
+      const message = 'user init rollback failed';
+        + (bucketDeleted ? ' bucket was deleted ' : ` bucket ${bucketId} was not deleted `);
+        + (userDeleted ? ' user was deleted ' : ` user ${userId} was not deleted `)
+        + `: ${(err as Error).message}`;
+
+      return new Error(message);
     }
   }
 
