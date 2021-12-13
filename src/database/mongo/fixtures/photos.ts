0954cc839972feac70add266c7c9eec39eb6965a
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2021-12-13 09:33:07 +0100

    use user fixtures on devices & photos fixtures

--- src/database/mongo/fixtures/photos.ts
+++ src/database/mongo/fixtures/photos.ts
@@ -1,10 +1,15 @@
 import { ObjectId } from 'mongodb';
+import { DeviceDocument } from '../models/Device';
 
 import { PhotoDocument } from '../models/Photo';
-import { users } from '../users';
+import { UserDocument } from '../models/User';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const devices = require('./devices');
+const users: UserDocument[] = require('./users');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const devices: DeviceDocument[] = require('./devices');
+
+const [userOne, userTwo] = users;
 const [userOneDeviceOne, , userTwoDeviceOne, userTwoDeviceTwo] = devices;
 
 const userOnePhotos: Required<PhotoDocument>[] = [
@@ -12,28 +17,28 @@ const userOnePhotos: Required<PhotoDocument>[] = [
     _id: new ObjectId('aaaaaaaaaaaaaaaaaaaaaaaa'),
     createdAt: new Date('2021-11-16 18:32:45.110Z'),
     updatedAt: new Date('2021-11-16 18:32:45.110Z'),
-    deviceId: userOneDeviceOne._id.toString(),
+    deviceId: userOneDeviceOne._id,
     fileId: 'photoOneFileId',
     heigth: 10,
     name: 'photoOne',
     previewId: 'photoOnePreviewId',
     size: 10,
     type: 'jpg',
-    userUuid: users.one,
+    userId: userOne._id,
     width: 10,
   },
   {
     _id: new ObjectId('aaaaaaaaaaaaaaaaaaaaaaab'),
     createdAt: new Date('2021-11-16 18:32:45.110Z'),
     updatedAt: new Date('2021-11-16 18:32:45.110Z'),
-    deviceId: userOneDeviceOne._id.toString(),
+    deviceId: userOneDeviceOne._id,
     fileId: 'photoTwoFileId',
     heigth: 10,
     name: 'photoTwo',
     previewId: 'photoTwoPreviewId',
     size: 10,
     type: 'jpg',
-    userUuid: users.one,
+    userId: userOne._id,
     width: 10,
   },
 ];
@@ -43,28 +48,28 @@ const userTwoPhotos: Required<PhotoDocument>[] = [
     _id: new ObjectId('aaaaaaaaaaaaaaaaaaaaaaac'),
     createdAt: new Date('2021-11-16 18:32:45.110Z'),
     updatedAt: new Date('2021-11-16 18:32:45.110Z'),
-    deviceId: userTwoDeviceOne._id.toString(),
+    deviceId: userTwoDeviceOne._id,
     fileId: 'photoThirdFileId',
     heigth: 10,
     name: 'photoThird',
     previewId: 'photoThirdPreviewId',
     size: 10,
     type: 'jpg',
-    userUuid: users.one,
+    userId: userTwo._id,
     width: 10,
   },
   {
     _id: new ObjectId('aaaaaaaaaaaaaaaaaaaaaaad'),
     createdAt: new Date('2021-11-16 18:32:45.110Z'),
     updatedAt: new Date('2021-11-16 18:32:45.110Z'),
-    deviceId: userTwoDeviceTwo._id.toString(),
+    deviceId: userTwoDeviceTwo._id,
     fileId: 'photoFourthFileId',
     heigth: 10,
     name: 'photoFourth',
     previewId: 'photoFourthPreviewId',
     size: 10,
     type: 'jpg',
-    userUuid: users.one,
+    userId: userTwo._id,
     width: 10,
   },
 ];
