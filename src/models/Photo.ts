65d5ceff18bf3220d3e834bd60bf8a0023ee4ffe
Sergio Gutiérrez <34506328+sg-gs@users.noreply.github.com>
2022-10-04 13:30:40 +0200

    Update Photo.ts

--- src/models/Photo.ts
+++ src/models/Photo.ts
@@ -8,30 +8,30 @@ export type PhotoId = string;
 export enum PhotoStatus {
   Exists = 'EXISTS',
   Trashed = 'TRASHED',
-  Deleted = 'DELETED'
+  Deleted = 'DELETED',
 }
 
 export interface Photo {
-  id: PhotoId
-  name: string
-  type: PhotoType
-  size: number
-  width: number
-  height: number
-  fileId: FileId
-  previewId: FileId
-  previews?: { width: number; height: number; size: number; fileId: FileId; type: PhotoPreviewType }[]
-  deviceId: DeviceId
-  userId: UserId
-  status: PhotoStatus
+  id: PhotoId;
+  name: string;
+  type: PhotoType;
+  size: number;
+  width: number;
+  height: number;
+  fileId: FileId;
+  previewId: FileId;
+  previews?: { width: number; height: number; size: number; fileId: FileId; type: PhotoPreviewType }[];
+  deviceId: DeviceId;
+  userId: UserId;
+  status: PhotoStatus;
   hash: string;
-  statusChangedAt: Date
-  takenAt: Date
-  duration?: number
-  itemType: PhotosItemType
+  statusChangedAt: Date;
+  takenAt: Date;
+  duration?: number;
+  itemType: PhotosItemType;
 }
 
-export type NewPhoto = Omit<Photo, 'id' | 'statusChangedAt' | 'status'>;
+export type NewPhoto = Omit<Photo, 'id' | 'statusChangedAt' | 'status'> & { networkBucketId: string };
 
 export enum PhotoPreviewType {
   PNG = 'PNG',
