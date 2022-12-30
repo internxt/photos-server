5c2b70651d2b37e2b0fc15c3375dee3210a5c968
Sergio Gutierrez Villalba <gv.sergio@gmail.com>
2022-12-30 12:37:25 +0100

    feat(photos): remove count from photos get

--- src/api/photos/usecase.ts
+++ src/api/photos/usecase.ts
@@ -36,20 +36,17 @@ export class PhotosUsecase {
 
   async get(
     userUuid: string,
-    filter: {name?: string, status?: PhotoStatus, statusChangedAt?: Date, deviceId?: string },
+    filter: {name?: string, status?: PhotoStatus, updatedAt?: Date, deviceId?: string },
     skip: number,
     limit: number,
-  ): Promise<{results: Photo[], count: number}> {
+  ): Promise<Photo[]> {
     const user = await this.usersRepository.getByUuid(userUuid);
 
     if (!user) {
       throw new UsecaseError(`User with uuid ${userUuid} does not exist`);
     }
     
-    const results = await this.photosRepository.get({userId: user.id, ...filter}, skip, limit);
-    const count = await this.photosRepository.count({userId: user.id, ...filter});
-
-    return { results, count };
+    return this.photosRepository.get({ userId: user.id, ...filter }, skip, limit);
   }
 
   async photosWithTheseCharacteristicsExist(
